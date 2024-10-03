5dbdaa59ccba39d9bb52e47ff55af1e40f7b315b
Asem Shaath <shaathasem@gmail.com>
2024-10-02 20:30:01 -0400

    finsihed processing videos

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,38 @@
 import express from 'express';
+import ffmpeg from 'fluent-ffmpeg';
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//req: express.Request, res: express.Response
+app.post('/process-video', (req: any, res: any) => {
+
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath){
+        console.log('files dne')
+        return res.sendStatus(400).send('Bad Request: Path is missing');
+    }
+
+    ffmpeg(inputFilePath)    
+        .outputOptions('-vf', 'scale=-1:360') // 360p
+        .on('end', ()=>{
+            console.log('Processing finished successfully');
+            return res.status(200).send('Processing finished successfully');    
+        })
+        .on('error', (err: any)=>{
+            console.log(`Error occured: ${err}`)
+            return res.status(200).send('Server error');    
+        })
+        .save(outputFilePath)
+
 });
 
+
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
